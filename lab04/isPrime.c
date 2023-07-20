eb42b0810613a632d8a780b1b112a0fa15bdcea0
jirametoon <sendtocartoon@gmail.com>
2023-07-20 12:00:23 +0700

    add isPrime and Makefile

---
+++ lab04/isPrime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int	isPrime(int num)
+{
+	if (num == 2)
+		return (1); // O(1)
+	else if (!(num % 2) || num < 2)
+		return (0); // O(1)
+	for (int i = 1; (2*i + 1) * (2*i + 1) <= num; i++)
+		if (!(num % (2*i + 1)))
+			return (0); // O(sqrt(n))
+	return (1);
+}
+
+int	main(void)
+{
+	int	num;
+
+	scanf("%d", &num);
+	printf("%d\n", isPrime(num));
+	return (0);
+}
+
+/*	
+	1 + 1 + sqrt(n)/2
+	O(1) + O(1) + O(sqrt(n)) = O(sqrt(n))
+*/
