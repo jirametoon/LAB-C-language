c208ef5217ce4f0d8984e6dfbd1af24e3251818e
jirametoon <sendtocartoon@gmail.com>
2023-09-01 01:58:31 +0700

    uvuvwevwevwe onyetenyevwe ugwemuhwem osas

--- lab07/basicTree.c
+++ lab07/basicTree.c
@@ -109,12 +109,22 @@ int	search(tree_t *t, int node)
 	return (find_node(t, node) != NULL);
 }
 
-/*
 int	degree(tree_t *t, int node)
 {
+	int	count;
 
+	t = find_node(t, node);
+	if (!t)
+		return (0);
+	t = t -> first_child;
+	count = 0;
+	while (t)
+	{
+		count++;
+		t = t -> next_sibling;
+	}
+	return (count);
 }
-*/
 
 int	is_root(tree_t *t, int node)
 {
@@ -177,7 +187,7 @@ int	main(void)
 			case 1:	scanf("%d %d", &parent, &child); t = attach(t, parent, child); break;
 			//case 2: scanf("%d", &node); t = detach(t, node); break;
 			case 3: scanf("%d", &node); printf("%d\n", search(t, node)); break;
-			//case 4: scanf("%d", &node); printf("%d\n", degree(t, node)); break;
+			case 4: scanf("%d", &node); printf("%d\n", degree(t, node)); break;
 			case 5: scanf("%d", &node); printf("%d\n", is_root(t, node)); break;
 			case 6: scanf("%d", &node); printf("%d\n", is_leaf(t, node)); break;
 			case 7: scanf("%d", &node); siblings(t, node); break;
